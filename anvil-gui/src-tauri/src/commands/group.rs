77fad638ee9a88e67f17a5002ae42d48bd6401ea
Ferdinand Laurel Edison <ferdinand@laureledison.org>
2026-08-14 16:42:06 +0700

    sync

--- anvil-gui/src-tauri/src/commands/group.rs
+++ anvil-gui/src-tauri/src/commands/group.rs
@@ -1,4 +1,6 @@
+use anvil_core::vault::groups::NewGroup;
 use tauri::State;
+use uuid::Uuid;
 
 use crate::{
     dto::{group_data_to_group_dto, GroupDto},
@@ -6,7 +8,8 @@ use crate::{
 };
 
 #[tauri::command]
-pub fn update_group(state: State<'_, AppState>, id: String) -> Result<(), AppError> {
+pub fn update_group(state: State<'_, AppState>, group_id: Uuid) -> Result<(), AppError> {
+    //TODO
     let mut guard = state.get_vault()?;
     let vault = guard.as_mut().ok_or(AppError::VaultNone)?;
     let master_guard = state.get_master_password()?;
@@ -17,22 +20,29 @@ pub fn update_group(state: State<'_, AppState>, id: String) -> Result<(), AppErr
     Ok(())
 }
 #[tauri::command]
-pub fn delete_group(state: State<'_, AppState>, id: String) -> Result<(), AppError> {
+pub fn delete_group(state: State<'_, AppState>, group_id: Uuid) -> Result<(), AppError> {
     let mut guard = state.get_vault()?;
     let vault = guard.as_mut().ok_or(AppError::VaultNone)?;
     let master_guard = state.get_master_password()?;
     let master_password = master_guard.as_str();
+    vault
+        .delete_group(group_id)
+        .map_err(|e| AppError::GroupDelete(e.to_string()))?;
     vault
         .save(master_password)
         .map_err(|e| AppError::VaultSave(e.to_string()))?;
     Ok(())
 }
 #[tauri::command]
-pub fn create_group(state: State<'_, AppState>) -> Result<(), AppError> {
+pub fn create_group(state: State<'_, AppState>, name: String) -> Result<(), AppError> {
     let mut guard = state.get_vault()?;
     let vault = guard.as_mut().ok_or(AppError::VaultNone)?;
     let master_guard = state.get_master_password()?;
     let master_password = master_guard.as_str();
+    let new_group = NewGroup::new(name);
+    vault
+        .add_group(new_group)
+        .map_err(|e| AppError::GroupCreate(e.to_string()))?;
     vault
         .save(master_password)
         .map_err(|e| AppError::VaultSave(e.to_string()))?;
